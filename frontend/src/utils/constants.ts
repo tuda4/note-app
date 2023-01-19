d4cd370da89167408cec0e909ec949cfad7b8a19
imtuda.dev <110141877+tuda4@users.noreply.github.com>
2023-01-20 05:05:39 +0700

    Update constants.ts

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = "https://note-app-9ik7.vercel.app/"
-export const GRAPHQL_SUBSCRIPTION_GRAPHQL ="https://note-app-9ik7.vercel.app/login"
+export const GRAPHQL_SUBSCRIPTION_GRAPHQL ="https://note-app-9ik7.vercel.app/graphql"
