ea9935c91d846d00aff6d9e0e01511ac3798d542
imtuda.dev <110141877+tuda4@users.noreply.github.com>
2023-01-20 10:33:14 +0700

    edit url

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = "https://note-app-tuda4-server.onrender.com"
-export const GRAPHQL_SUBSCRIPTION_GRAPHQL ="ws://note-app-tuda4-server.onrender.com/"
+export const GRAPHQL_SUBSCRIPTION_GRAPHQL ="ws://note-app-tuda4-server.onrender.com/graphql"
